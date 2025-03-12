ed0c96aebe81dbe9446fb3bd36ae86538adf9db3
Steven Young <ywch2002@sjtu.edu.cn>
2025-03-12 15:19:04 +0800

    idenpotence

--- src/main/java/com/wortox/quizai/model/dto/userAnswer/UserAnswerAddRequest.java
+++ src/main/java/com/wortox/quizai/model/dto/userAnswer/UserAnswerAddRequest.java
@@ -1,7 +1,5 @@
 package com.wortox.quizai.model.dto.userAnswer;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -9,13 +7,11 @@ import java.util.List;
 
 /**
  * 创建用户答案请求
- *
-
-
  */
 @Data
 public class UserAnswerAddRequest implements Serializable {
 
+    private Long id;
     /**
      * 应用 id
      */
