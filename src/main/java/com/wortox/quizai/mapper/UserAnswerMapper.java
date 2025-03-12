b82da0054dbd3fcdc5d7c5f50f4b344c46ba80f1
Steven Young <ywch2002@sjtu.edu.cn>
2025-03-12 13:33:24 +0800

    init

---
+++ src/main/java/com/wortox/quizai/mapper/UserAnswerMapper.java
@@ -0,0 +1,18 @@
+package com.wortox.quizai.mapper;
+
+import com.wortox.quizai.model.entity.UserAnswer;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 李鱼皮
+* @description 针对表【user_answer(用户答题记录)】的数据库操作Mapper
+* @createDate 2024-05-09 20:41:03
+* @Entity com.wortox.quizai.model.entity.UserAnswer
+*/
+public interface UserAnswerMapper extends BaseMapper<UserAnswer> {
+
+}
+
+
+
+
---
+++ src/main/java/com/wortox/quizai/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.wortox.quizai.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wortox.quizai.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
