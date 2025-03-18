1d071eb1a8b2dd33ce386f5d8fb21965c5bada13
Steven Young <ywch2002@sjtu.edu.cn>
2025-03-18 16:01:46 +0800

    nacos

--- src/main/java/com/wortox/quizai/MainApplication.java
+++ src/main/java/com/wortox/quizai/MainApplication.java
@@ -4,20 +4,19 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 主类（项目启动入口）
- *
-
-
  */
 // todo 如需开启 Redis，须移除 exclude 中的内容
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class})
 @MapperScan("com.wortox.quizai.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@ServletComponentScan
 public class MainApplication {
 
     public static void main(String[] args) {
